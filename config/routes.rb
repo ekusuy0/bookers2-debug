7f91d1af11aa12d45e567a131d46582fc50aa088
ekusuy0 <105528947+ekusuy0@users.noreply.github.com>
2022-07-05 12:09:44 +0000

    [Update] フォロー機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+  
   root :to =>"homes#top"
   get "home/about"=>"homes#about"
-
+  
   devise_for :users
-
+  
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update]
-  resources :users, only: [:index,:show,:edit,:update]
+  resources :users do
+    resource :relationships, only: [:create, :destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
